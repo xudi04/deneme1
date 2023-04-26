30117b90cc801d851ff98996ec2ec385b652f3ef
xudi04 <129602928+xudi04@users.noreply.github.com>
2023-04-26 16:45:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
-const adminRouter = require("./routers/adminRouters");
-const blogRouter = require("./routers/blogRouters");
 
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended : true}));
-app.use(express.static('public'));
 
-const dbURL = "mongodb+srv://musti1:musti123@aqil.hq9pwf5.mongodb.net/test";
-mongoose.connect(dbURL)
-  .then((res) =>{
-      console.log("bağlandi")
-      app.listen(3000, () => {
+
+app.listen(3000, () => {
         console.log("http://localhost:3000");
         console.log("strated...");
       });
-  })
-  .catch((err) => console.log(err));
 
 
 app.get('/', function (req, res) {
-  res.redirect("/blog");
+  res.send("/blog");
 });
 
-app.use("/admin", adminRouter);
-app.use("/blog" ,blogRouter);
 
 
 app.get('/about', function (req, res) {
